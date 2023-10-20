aa44f99088e79de372cd4b8d092de6ad1a1e2d9a
Sander Roest <s.roest@dalosy.com>
2023-10-20 11:01:55 +0200

    Add the expected discovery classes

---
+++ lib/src/classes/discovery/discovery_advanced.dart
@@ -0,0 +1,3 @@
+import 'package:zebra_discovery_b64/src/classes/discovery/discovery.dart';
+
+class DiscoveryAdvanced extends Discovery {}
--- lib/src/zebra_discovery_b64_base.dart
+++ lib/src/zebra_discovery_b64_base.dart
@@ -1,5 +1,8 @@
 //This function decodes a discoveryB64 message
 
-Object? decodeDiscoveryB64(String discoveryB64) {
-  return true;
+import 'package:zebra_discovery_b64/src/classes/discovery/discovery.dart';
+import 'package:zebra_discovery_b64/src/classes/discovery/discovery_advanced.dart';
+
+Discovery decodeDiscoveryB64(String discoveryB64) {
+  return DiscoveryAdvanced();
 }
