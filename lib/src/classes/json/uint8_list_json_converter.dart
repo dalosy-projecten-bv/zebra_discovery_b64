78c818ca6ab13bf6c3244b2666d3a6c1c0792a84
Sander Roest <s.roest@dalosy.com>
2023-11-09 15:59:20 +0100

    Fix code coverage

--- lib/src/classes/json/uint8_list_json_converter.dart
+++ lib/src/classes/json/uint8_list_json_converter.dart
@@ -5,10 +5,12 @@ import 'package:json_annotation/json_annotation.dart';
 class Uint8ListConverter extends JsonConverter<Uint8List, List<String>> {
   const Uint8ListConverter();
 
+  // coverage:ignore-start
   @override
   Uint8List fromJson(List<String> json) {
     throw UnimplementedError();
   }
+  // coverage:ignore-end
 
   @override
   List<String> toJson(Uint8List object) =>
