06f42774e8948220e8fe46c42dd0182172b06cef
Sander Roest <s.roest@dalosy.com>
2023-11-09 15:59:12 +0100

    Use toRadixString instead of self made byte class

--- lib/src/classes/json/uint8_list_json_converter.dart
+++ lib/src/classes/json/uint8_list_json_converter.dart
@@ -2,65 +2,15 @@ import 'dart:typed_data';
 
 import 'package:json_annotation/json_annotation.dart';
 
-class Byte {
-  Byte(
-    this.bit0,
-    this.bit1,
-    this.bit2,
-    this.bit3,
-    this.bit4,
-    this.bit5,
-    this.bit6,
-    this.bit7,
-  );
-
-  final bool bit0;
-  final bool bit1;
-  final bool bit2;
-  final bool bit3;
-  final bool bit4;
-  final bool bit5;
-  final bool bit6;
-  final bool bit7;
-
-  int toBit(bool bit) => bit ? 1 : 0;
-
-  int toInt() =>
-      toBit(bit0) * 1 +
-      toBit(bit1) * 2 +
-      toBit(bit2) * 4 +
-      toBit(bit3) * 8 +
-      toBit(bit4) * 16 +
-      toBit(bit5) * 32 +
-      toBit(bit6) * 64 +
-      toBit(bit7) * 128;
-
-  factory Byte.fromInt(int value) {
-    return Byte(
-      value & 1 != 0,
-      value & 2 != 0,
-      value & 4 != 0,
-      value & 8 != 0,
-      value & 16 != 0,
-      value & 32 != 0,
-      value & 64 != 0,
-      value & 128 != 0,
-    );
-  }
-
-  String toJson() =>
-      "${toBit(bit7)}${toBit(bit6)}${toBit(bit5)}${toBit(bit4)}${toBit(bit3)}${toBit(bit2)}${toBit(bit1)}${toBit(bit0)}";
-}
-
-class Uint8ListConverter extends JsonConverter<Uint8List, List<Byte>> {
+class Uint8ListConverter extends JsonConverter<Uint8List, List<String>> {
   const Uint8ListConverter();
 
   @override
-  Uint8List fromJson(List<Byte> json) {
+  Uint8List fromJson(List<String> json) {
     throw UnimplementedError();
   }
 
   @override
-  List<Byte> toJson(Uint8List object) =>
-      object.map((e) => Byte.fromInt(e)).toList();
+  List<String> toJson(Uint8List object) =>
+      object.map((e) => e.toRadixString(2).padLeft(8, '0')).toList();
 }
